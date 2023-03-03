e95bb6fb4b23e63bbb8c7f3bd5224d99ce8bbc52
teamprojects01 <125862266+teamprojects01@users.noreply.github.com>
2023-03-03 15:48:53 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
   const [option, setOption] = useState();
 
   const onSubmit = (data) =>
-    setLogin(data.username === "root" && data.password === "12345");
+    setLogin(data.username === "fclfc" && data.password === "fclfc@123");
 
   return (
     <div className="App">
